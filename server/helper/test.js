7a3ab064fe079db1e508fd00d5f161dc61d2422d
santeripasula1@gmail.com <santeripasula1@gmail.com>
2024-11-11 23:58:48 +0200

    todoapp

---
+++ server/helper/test.js
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import path from 'path';
+import { pool } from './db.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const __dirname = import.meta.dirname
+
+const initializeTestDb = () => {
+    const sql = fs.readFileSync(path.resolve(__dirname,'../todo.sgl'), 'utf8');
+    pool.query(sql)
+}
+
+const insertTestUser = (email, password) => {
+    hash(password,10,(error,hashedPassword) => {
+        pool.query('insert into account (email,password) values ($1,$2)',
+            [email,hashedPassword])
+            })   
+    }
+
+const getToken = (email) => {
+    return sign({user: email},process.env.JWT_SECRET_KEY)
+}
+
+export { initializeTestDb, insertTestUser, getToken }
